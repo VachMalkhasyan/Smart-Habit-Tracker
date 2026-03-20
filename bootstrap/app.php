ac2393be19202c0737c0ce8f792e3d10263dc854
Vach <vachmalkhasyan@gmail.com>
2026-03-20 18:10:02 +0400

    fix: deployment fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,14 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             \App\Http\Middleware\RedirectIfNotOnboarded::class,
-
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->respond(function (Response|\Symfony\Component\HttpFoundation\Response $response, \Throwable $e, Request $request) {
